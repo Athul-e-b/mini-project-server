8287ec5f348d48894cf129a2ad0cb376f12cb02c
BINIL E B <binilbalan.e.k@gmail.com>
2023-06-03 18:54:49 +0530

    login and registration completed

--- registration.php
+++ registration.php
@@ -1,10 +1,30 @@
 <?php
-header('Access-Control-Allow-Origin:*');
+include_once("connection.php");
+header('Access-Control-Allow-Origin: *');
 header('Allow-Access-Control-Method:POST,GET');
 
-$request=json_decode($_POST['data']);
-
+$request=json_decode($_POST['body']); 
 $obj = new stdClass();
-$obj->key = $request->name;
-echo json_encode($obj);
+
+$fetchData = "SELECT * FROM users WHERE email='$request->email' or phone='$request->phone'";
+$result = mysqli_query($connect,$fetchData);
+
+if(mysqli_num_rows($result) == 0){
+    $insertData = "INSERT INTO `users`(`name`, `email`, `phone`, `password`) VALUES ('$request->name','$request->email','$request->phone','$request->password')";
+     if(mysqli_query($connect,$insertData)){
+        $obj->message = 'Registration successfull';
+        $obj->status = true;
+        echo json_encode($obj);
+     }else{
+        $obj->message = 'Registration failed';
+        $obj->status = false;
+        echo json_encode($obj);
+     }
+}else{
+    $obj->message = 'User already exist';
+    $obj->status = false;
+    echo json_encode($obj);
+  
+}
+
 ?>
